6f2afd1add2509596dc5dfd156cc251e68f01831
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-06-29 11:49:07 +0900

    table view

---
+++ SampleApp/SampleApp/BountyTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BoutyTableViewCell.swift
+//  SampleApp
+//
+//  Created by 이형진 on 2022/06/29.
+//
+
+import UIKit
+
+class BountyTableViewCell: UITableViewCell {
+    @IBOutlet var photoImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var bountyLabel: UILabel!
+}
---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by 이형진 on 2022/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    let bountyList : [String] = ["luffy","brook","nami","zoro","sanji"]
+    let bountyDict : [String:Int] = ["luffy":30000,"brook":2000,"nami":1000,"zoro":20000,"sanji":15000]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bountyList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? BountyTableViewCell else{
+            return UITableViewCell()
+        }
+        
+        let name = bountyList[indexPath.row]
+        cell.nameLabel.text = name
+        cell.bountyLabel.text = String(bountyDict[name]!)
+        cell.photoImage.image = UIImage(named: "\(name).jpg")
+        return cell
+    }
+
+    
+}
+
