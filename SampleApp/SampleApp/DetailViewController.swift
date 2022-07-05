cca7dd322368392dcb59b4a426b01428220cf90e
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-07-05 11:52:52 +0900

    animation

--- SampleApp/SampleApp/DetailViewController.swift
+++ SampleApp/SampleApp/DetailViewController.swift
@@ -12,10 +12,20 @@ class DetailViewController: UIViewController {
     @IBOutlet var bountyLabel: UILabel!
     @IBOutlet var photoImage: UIImageView!
     
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
+    
     let viewmodel = DetailViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
     }
     
     func updateUI(){
@@ -23,11 +33,26 @@ class DetailViewController: UIViewController {
             photoImage.image = bountyInfo.image
             nameLabel.text = bountyInfo.name
             bountyLabel.text = String(bountyInfo.bounty)
-
         }
     }
     @IBAction func touchUpCloseButton(_ sender: UIButton){
         dismiss(animated: true, completion: nil)
+    }
+    
+    func prepareAnimation(){
+        nameLabelCenterX.constant = view.bounds.width
+        bountyLabelCenterX.constant = view.bounds.width
+    }
+    
+    func showAnimation(){
+        nameLabelCenterX.constant=0
+        bountyLabelCenterX.constant=0
+
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+        })
+        
+        UIView.transition(with: photoImage, duration: 0.3, options: .transitionFlipFromTop, animations: nil, completion: nil)
     }
 }
 
